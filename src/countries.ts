64b5df4c8f8f5bb7d1f30ba8576f5f410123a1de
shock59 <42130427+shock59@users.noreply.github.com>
2025-12-23 10:40:25 +1100

    Start list of countries

---
+++ src/countries.ts
@@ -0,0 +1,81 @@
+type Country = {
+  name: string;
+  iso: string;
+};
+
+const countries: Country[] = [
+  {
+    name: "Afghanistan",
+    iso: "af",
+  },
+  {
+    name: "Albania",
+    iso: "al",
+  },
+  {
+    name: "Algeria",
+    iso: "dz",
+  },
+  {
+    name: "Andorra",
+    iso: "ad",
+  },
+  {
+    name: "Angola",
+    iso: "ao",
+  },
+  {
+    name: "Antigua and Barbuda",
+    iso: "ag",
+  },
+  {
+    name: "Argentina",
+    iso: "ar",
+  },
+  {
+    name: "Armenia",
+    iso: "am",
+  },
+  {
+    name: "Australia",
+    iso: "au",
+  },
+  {
+    name: "Austria",
+    iso: "at",
+  },
+  {
+    name: "Azerbaijan",
+    iso: "az",
+  },
+  {
+    name: "Bahrain",
+    iso: "bh",
+  },
+  {
+    name: "Bangladesh",
+    iso: "bd",
+  },
+  {
+    name: "Barbados",
+    iso: "bb",
+  },
+  {
+    name: "Belarus",
+    iso: "by",
+  },
+  {
+    name: "Belgium",
+    iso: "be",
+  },
+  {
+    name: "Belize",
+    iso: "bz",
+  },
+  {
+    name: "Benin",
+    iso: "bj",
+  },
+];
+
+export default countries;
