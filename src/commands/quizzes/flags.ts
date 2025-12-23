a8a309370661c12b6ac0d76f6db07260bd3b1acf
shock59 <42130427+shock59@users.noreply.github.com>
2025-12-23 12:45:14 +1100

    Add interaction to the flags quiz

--- src/commands/quizzes/flags.ts
+++ src/commands/quizzes/flags.ts
@@ -2,12 +2,14 @@ import {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  ComponentType,
   ContainerBuilder,
   MediaGalleryBuilder,
   MediaGalleryItemBuilder,
   MessageFlags,
   SlashCommandSubcommandBuilder,
   TextDisplayBuilder,
+  User,
   type MessageActionRowComponentBuilder,
 } from "discord.js";
 import { multipleRandomFromArray, shuffleArray } from "../../lib/arrays.js";
@@ -17,7 +19,7 @@ const flags: Subcommand = {
   data: new SlashCommandSubcommandBuilder()
     .setName("flags")
     .setDescription("Do a flag quiz with others in the server!"),
-  execute(interaction) {
+  async execute(interaction) {
     const choices = multipleRandomFromArray(countries, 4, true);
     const correctCountry = choices[0]!;
     const shuffledChoices = shuffleArray(choices);
@@ -25,7 +27,7 @@ const flags: Subcommand = {
     const components = [
       new ContainerBuilder()
         .addTextDisplayComponents(
-          new TextDisplayBuilder().setContent("# Guess the country flag:")
+          new TextDisplayBuilder().setContent("## Guess the country flag:")
         )
         .addMediaGalleryComponents(
           new MediaGalleryBuilder().addItems(
@@ -46,9 +48,76 @@ const flags: Subcommand = {
         ),
     ];
 
-    interaction.reply({
+    const response = await interaction.reply({
       components,
       flags: MessageFlags.IsComponentsV2,
+      withResponse: true,
+    });
+
+    if (!response.resource?.message) return;
+
+    let responses: {
+      user: User;
+      choice: string;
+    }[] = [];
+
+    const collector = response.resource.message.createMessageComponentCollector(
+      {
+        componentType: ComponentType.Button,
+        time: 15_000,
+      }
+    );
+    collector.on("collect", async (interaction) => {
+      if (
+        responses.some((response) => response.user.id == interaction.user.id)
+      ) {
+        await interaction.reply({
+          content: "You already guessed!",
+          flags: MessageFlags.Ephemeral,
+        });
+        return;
+      }
+
+      responses.push({
+        user: interaction.user,
+        choice: interaction.customId,
+      });
+
+      const chosenCountry = choices.find(
+        (country) => country.iso == interaction.customId
+      );
+      await interaction.reply({
+        content: `You guessed **${chosenCountry?.name}**!`,
+        flags: MessageFlags.Ephemeral,
+      });
+    });
+
+    collector.on("end", async () => {
+      const components = [
+        new ContainerBuilder().addTextDisplayComponents(
+          new TextDisplayBuilder().setContent(
+            `The correct answer was **${correctCountry.name}**\n\n${responses
+              .map((response) => {
+                const correct = response.choice == correctCountry.iso;
+                const chosenCountry = choices.find(
+                  (country) => country.iso == response.choice
+                );
+
+                return `:${
+                  correct ? "white_check_mark" : "x"
+                }: ${response.user.toString()} selected **${
+                  chosenCountry?.name
+                }**`;
+              })
+              .join("\n")}`
+          )
+        ),
+      ];
+
+      await interaction.editReply({
+        components,
+        flags: MessageFlags.IsComponentsV2,
+      });
     });
   },
 };
