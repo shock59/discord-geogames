a99ebf7072e0f31b4cfd8d9e64994e277ac03649
shock59 <42130427+shock59@users.noreply.github.com>
2025-12-23 22:37:15 +1100

    Improve appearance of flag quiz

--- src/commands/quizzes/flags.ts
+++ src/commands/quizzes/flags.ts
@@ -7,19 +7,28 @@ import {
   MediaGalleryBuilder,
   MediaGalleryItemBuilder,
   MessageFlags,
+  SectionBuilder,
+  SeparatorBuilder,
   SlashCommandSubcommandBuilder,
   TextDisplayBuilder,
+  ThumbnailBuilder,
   User,
   type MessageActionRowComponentBuilder,
 } from "discord.js";
 import { multipleRandomFromArray, shuffleArray } from "../../lib/arrays.js";
 import countries from "../../countries.js";
 
+function flagUrl(iso: string) {
+  return `https://flagcdn.com/h240/${iso}.webp`;
+}
+
 const flags: Subcommand = {
   data: new SlashCommandSubcommandBuilder()
     .setName("flags")
     .setDescription("Do a flag quiz with others in the server!"),
   async execute(interaction) {
+    const time = 15;
+
     const choices = multipleRandomFromArray(countries, 4, true);
     const correctCountry = choices[0]!;
     const shuffledChoices = shuffleArray(choices);
@@ -31,9 +40,7 @@ const flags: Subcommand = {
         )
         .addMediaGalleryComponents(
           new MediaGalleryBuilder().addItems(
-            new MediaGalleryItemBuilder().setURL(
-              `https://flagcdn.com/h240/${correctCountry.iso}.webp`
-            )
+            new MediaGalleryItemBuilder().setURL(flagUrl(correctCountry.iso))
           )
         )
         .addActionRowComponents(
@@ -44,6 +51,17 @@ const flags: Subcommand = {
                 .setLabel(country.name)
                 .setCustomId(country.iso)
             )
+          )
+        )
+        .addTextDisplayComponents(
+          new TextDisplayBuilder().setContent(
+            `Ending <t:${Math.floor(Date.now() / 1000) + time}:R>`
+          )
+        )
+        .addSeparatorComponents(new SeparatorBuilder().setDivider(true))
+        .addTextDisplayComponents(
+          new TextDisplayBuilder().setContent(
+            "-# Flag images sourced from [Flagpedia.net](https://flagpedia.net)"
           )
         ),
     ];
@@ -64,9 +82,10 @@ const flags: Subcommand = {
     const collector = response.resource.message.createMessageComponentCollector(
       {
         componentType: ComponentType.Button,
-        time: 15_000,
+        time: time * 1000,
       }
     );
+
     collector.on("collect", async (interaction) => {
       if (
         responses.some((response) => response.user.id == interaction.user.id)
@@ -87,36 +106,58 @@ const flags: Subcommand = {
         (country) => country.iso == interaction.customId
       );
       await interaction.reply({
-        content: `You guessed **${chosenCountry?.name}**!`,
+        content: `You selected **${chosenCountry?.name}**!`,
         flags: MessageFlags.Ephemeral,
       });
     });
 
     collector.on("end", async () => {
       const components = [
-        new ContainerBuilder().addTextDisplayComponents(
-          new TextDisplayBuilder().setContent(
-            `The correct answer was **${correctCountry.name}**\n\n${responses
-              .map((response) => {
-                const correct = response.choice == correctCountry.iso;
-                const chosenCountry = choices.find(
-                  (country) => country.iso == response.choice
-                );
-
-                return `:${
-                  correct ? "white_check_mark" : "x"
-                }: ${response.user.toString()} selected **${
-                  chosenCountry?.name
-                }**`;
-              })
-              .join("\n")}`
+        new ContainerBuilder()
+          .addSectionComponents(
+            new SectionBuilder()
+              .setThumbnailAccessory(
+                new ThumbnailBuilder().setURL(flagUrl(correctCountry.iso))
+              )
+              .addTextDisplayComponents(
+                new TextDisplayBuilder().setContent(`The correct answer was`),
+                new TextDisplayBuilder().setContent(
+                  `### ${correctCountry.name}`
+                )
+              )
           )
-        ),
+          .addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+              responses.length
+                ? `${responses
+                    .map((response) => {
+                      const correct = response.choice == correctCountry.iso;
+                      const chosenCountry = choices.find(
+                        (country) => country.iso == response.choice
+                      );
+
+                      return `:${
+                        correct ? "white_check_mark" : "x"
+                      }: ${response.user.toString()} answered **${
+                        chosenCountry?.name
+                      }**`;
+                    })
+                    .join("\n")}`
+                : "Nobody chose an answer."
+            )
+          )
+          .addSeparatorComponents(new SeparatorBuilder().setDivider(true))
+          .addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+              "-# Flags sourced from [Flagpedia.net](https://flagpedia.net)"
+            )
+          ),
       ];
 
       await interaction.editReply({
         components,
         flags: MessageFlags.IsComponentsV2,
+        allowedMentions: { parse: [] },
       });
     });
   },
